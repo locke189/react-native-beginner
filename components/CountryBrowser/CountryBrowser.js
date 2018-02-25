4d3c0d67faf2fa045ffc56cbb7802535bceb5127
Juan David Insuasti Bravo <juan.insuasti@gmail.com>
2018-02-25 17:12:18 -0500

    Country browser component with API call

--- components/CountryBrowser/CountryBrowser.js
+++ components/CountryBrowser/CountryBrowser.js
@@ -5,14 +5,53 @@ import {
   Text,
   View
 } from 'react-native';
-
+import axios from 'axios';
 
 export default class CountryBrowser extends Component {
+  state = {regions: {}};
+
+  componentWillMount(){
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(({data}) => {
+        console.log(data);
+        this.setState({regions: this.createCountryDataStructure(data)}, () => console.log(this.state));
+      })
+      .catch(error => {
+        console.log(error.response)
+    });
+  }
+
+  createCountryDataStructure(countryList){
+    return countryList.reduce(this.reduceCountryListToRegionObject, {});
+  }
+
+  reduceCountryListToRegionObject(result, country){
+    const newResult = result;
+    if(newResult[country.region]){
+      newResult[country.region].push(country);
+    }
+    else{
+      newResult[country.region] = [country];
+    }
+    return newResult;
+  }
+
+  renderRegions(regions){
+    return Object.keys(regions)
+      .map(region => {(
+        <Text key={region}>{region}</Text>
+      )});
+  }
+
   render() {
+    console.log('state');
+    console.log(this.state.regions);
     return (
       <View style={styles.countryBrowser}>
         <Text style={styles.title}>
           CountryBrowser Component!
+          {this.renderRegions(this.state.regions)}
         </Text>
       </View>
     );
